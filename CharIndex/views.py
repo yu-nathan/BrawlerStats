1906cb7898166cfebe1dd3c357b719d29962fc5d
Nathan Yu <nyu1997@gmail.com>
2019-10-26 02:37:53 -0700

    Adding search bar to the homepage and all sites

--- CharIndex/views.py
+++ CharIndex/views.py
@@ -1,10 +1,12 @@
+# Views for the app charIndex.
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
+from django.db.models import Q
 
 from .models import Character, Stats
 
 # Create your views here.
-class CharacterDetails(generic.DetailView):
+class CharacterDetailsView(generic.DetailView):
     model = Stats
     template_name = 'charIndex/detail.html'
     context_object_name = 'character'
@@ -12,3 +14,13 @@ class CharacterDetails(generic.DetailView):
     def get_object(self):
         return get_object_or_404(Character, char_name=self.kwargs['char_name'],
             char_origin=self.kwargs['char_origin'])
+
+class SearchResultsView(generic.DetailView):
+    model = Character
+    template_name = 'charIndex/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Character.objects.filter(
+            Q(char_name__icontains=query) | Q(char_origin__icontains=query)
+        )
